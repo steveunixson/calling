69b585bf1b172d7486db35d86aa84d9e31a89437
steveunixson <unixson@gmail.com>
2018-03-03 17:36:53 +0300

    added new models

---
+++ app/models/comments.model.js
@@ -0,0 +1,30 @@
+var log = require('../../libs/log')(module);
+var config = require('config.json')('app/config/config.json');
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.set('debug', true);
+mongoose.connect(config.mongoose.uri, {
+    //useMongoClient: true,
+  });
+//-------------------ERROR HANDLER---------------//
+var db = mongoose.connection;
+
+db.on('error', function (err) {
+    log.error('connection error:', err.message);
+});
+db.once('open', function callback () {
+    log.info("Comments model active");
+});
+//-------------------ERROR HANDLER---------------//
+
+var Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
+ 
+var Comments = new Schema({
+    comment: String,
+    name: String,
+    base: String
+});
+
+var CommentModel = mongoose.model('Comments', Comments);
+module.exports = CommentModel;
